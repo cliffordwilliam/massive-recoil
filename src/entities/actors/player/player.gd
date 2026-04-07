4388ab452c2e34ba71fa5addc7451aff560d08d0
Clifford William <ccliffordwilliam@gmail.com>
2026-04-08 03:13:05 +0700

    fix: playe art refinement

--- src/entities/actors/player/player.gd
+++ src/entities/actors/player/player.gd
@@ -2,6 +2,6 @@ class_name Player
 extends CharacterBody2D
 ## The player character.
 
-const WALK_SPEED: float = 26.0
+const WALK_SPEED: float = 25.0
 
 @onready var body: ChainedAnimatedSprite2D = $Body
